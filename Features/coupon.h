070785a2edc7f81e197efa7d2e23eea4df391a43
GGital <sakukiheng@gmail.com>
2024-12-02 22:35:28 +0700

    Added coupon feature

---
+++ Features/coupon.h
@@ -0,0 +1,29 @@
+#ifndef Coupon
+#define Coupon
+
+typedef struct
+{
+    int id;
+    char name[150];
+    float minimum_cost;
+    float discountRate;
+    int expiry_day;
+    int expiry_month;
+    int expiry_year;
+} coupon;
+
+void coupon_load(coupon *coupons, const char *coupon_csv);
+
+void coupon_read(coupon *coupons, float min_discount, float max_discount);
+
+void coupon_save(coupon *coupons, const char *coupon_csv);
+
+void delete_expired_coupons(coupon *coupons);
+
+void coupon_create(coupon *coupons, const char *coupon_csv, char name[], float minimum_cost, float discountRate, int expiry_day, int expiry_month, int expiry_year);
+
+extern int coupon_count;
+
+extern int max_coupon_index;
+
+#endif
